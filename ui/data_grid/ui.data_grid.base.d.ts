415fc7b2119532f04829f757ccbdddd96ebbda06
Aldana Verón Ruíz Díaz <aldana@fluxus-iq.com>
2020-03-25 18:34:33 -0300

    master

--- ui/data_grid/ui.data_grid.base.d.ts
+++
@@ -1,9 +0,0 @@
-/**
-* DevExtreme (ui/data_grid/ui.data_grid.base.d.ts)
-* Version: 20.1.1
-* Build date: Wed Mar 25 2020
-*
-* Copyright (c) 2012 - 2020 Developer Express Inc. ALL RIGHTS RESERVED
-* Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
-*/
-export { GridBaseColumn as Column} from '../data_grid';
--- ui/scroll_view/scrollable.d.ts
+++
@@ -1,9 +0,0 @@
-/**
-* DevExtreme (ui/scroll_view/scrollable.d.ts)
-* Version: 20.1.1
-* Build date: Wed Mar 25 2020
-*
-* Copyright (c) 2012 - 2020 Developer Express Inc. ALL RIGHTS RESERVED
-* Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
-*/
-export { dxScrollableOptions as Options } from './ui.scrollable';
